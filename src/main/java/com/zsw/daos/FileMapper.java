fdfee8e152c8e8859265dda95e62594d92a1c7d9
zhangshaowei <zhangshaowei@iplas.com.cn>
2020-06-12 15:21:23 +0800

    new

--- src/main/java/com/zsw/daos/FileMapper.java
+++ src/main/java/com/zsw/daos/FileMapper.java
@@ -1,11 +1,15 @@
 package com.zsw.daos;
 
 import com.zsw.entitys.FileEntity;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Map;
 
+@Mapper
+@Component
 public interface FileMapper {
     Integer listFileEntityCount(@Param("paramMap") Map<String,Object> paramMap);
 
